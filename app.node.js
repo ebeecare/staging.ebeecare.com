ffd6b573985ef90adb217edc85a3dec685c4e345
Eric Lu <ericluwj@hotmail.com>
2016-03-21 13:28:44 +0800

    Update 2016-03-21T05:28:44.324Z

--- app.node.js
+++ app.node.js
@@ -7058,10 +7058,10 @@ module.exports =
               redirecting: true
             });
             console.log('Redirecting to ' + res.body.url);
-            window.location = res.body.url;
+            // window.location = res.body.url;
           } else {
-            console.error('Failed to create paypal payment.');
-          }
+              console.error('Failed to create paypal payment.');
+            }
         });
       }
     }]);
